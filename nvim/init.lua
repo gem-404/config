2478d2599ca162356b988aacd10fbb65c9ac288c
GEM-404 <mashaephantus2000@gmail.com>
2022-11-24 12:58:59 +0300

    Added nvim configurations

---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+
+require ("core/keymappings")
+require ("core/packer_init")
+require ("core/editor")
+
+-- something that comes close to this level of beauty should be much protected.
+
+vim.cmd("colorscheme synthwave84")
+-- vim.cmd("autocmd VimEnter * TZAtaraxis")
